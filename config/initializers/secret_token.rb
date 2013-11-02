90b19e97b1ef5833e945954fa8fe980ba20e29ef
Alexander Sologub <alexsologoub@gmail.com>
2013-11-02 14:59:36 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffinRails::Application.config.secret_key_base = '157e74d96e5ad35515600ff7cbf96029873379e9aad4477cb49325b481b43d47a9a6b07398498c3c2004e9f1f0727ad6b1ddb4b0227eb53b92377f0f47f001fa'
